f209ee6ca5aab50076b7c96f3d8dc96a71c26077
PengtaoJiang <pengtaojiang@163.com>
2021-06-12 22:32:31 +0800

    .

--- test_iam.sh
+++ test_iam.sh
@@ -8,6 +8,6 @@ CUDA_VISIBLE_DEVICES=0  python3 ./scripts/test_iam.py \
     --batch_size=1 \
     --dataset=pascal_voc \
     --input_size=224 \
-	  --num_classes=20 \
+	--num_classes=20 \
     --restore_from=./runs/${EXP}/model/pascal_voc_epoch_14.pth \
     --save_dir=./runs/${EXP}/attention/ \
